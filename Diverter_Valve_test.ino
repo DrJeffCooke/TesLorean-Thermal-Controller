247a06f5e08d51b39443f2b98815ee0607004df4
Jeff Cooke <24613307+DrJeffCooke@users.noreply.github.com>
2018-06-21 14:07:32 -0500

    Add files via upload

---
+++ Diverter_Valve_test.ino
@@ -0,0 +1,17 @@
+int intPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(intPin,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(intPin,HIGH);
+  delay(100);
+
+  digitalWrite(intPin,LOW);
+  delay(100);
+}
+
